d27cbfc6811915a0d4f28d5be0e054710e20d330
Tsifteteli <carbol-8@student.ltu.se>
2019-04-08 11:28:13 +0200

    Konstruktor User

--- src/BugTracker/User.java
+++ src/BugTracker/User.java
@@ -13,5 +13,12 @@ public class User {
    private String firstName;
    private String lastName;
    private UserDesignation designation;
-
+   
+   public User(String firstName, String lastName, UserDesignation designation){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.designation = designation;
+   }
+   
+   //setters och getters för samtliga instansvariabler.
 }
